b1199c8fc05ba1f05d9e1a4848c7a9ad29f8803a
Elly Logan  Odhiambo <98582398+Elly-L@users.noreply.github.com>
2025-11-30 23:10:59 +0300

    Update Firebase config environment variables

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// firebase-config.js
-export 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: `${import.meta.env.VITE_PROJECT_ID}.firebaseapp.com`,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: `${import.meta.env.VITE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
