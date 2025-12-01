63aa29eeeab9d56271991677166cd4c6740e9c9b
Elly Logan  Odhiambo <98582398+Elly-L@users.noreply.github.com>
2025-12-01 09:19:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD9rFzcijHsJi7YMw1jm72Yae6XDjQH9uI",
-  authDomain: "player-performance-track-cd851.firebaseapp.com",
-  projectId: "player-performance-track-cd851",
-  storageBucket: "player-performance-track-cd851.appspot.com",
-  messagingSenderId: "650433300915",
-  appId: "1:650433300915:web:597792cf7435537021a3c6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
